2d88e19d3b20add3d38bf56f430a89fac680a419
chandanbir <chandanbirsingh9@gmail.com>
2024-07-28 23:50:36 +0530

    Update .prettierignore and .prettierrc files, remove unused code, and update dependencies

--- client/src/partials/landing/Navbar.jsx
+++ client/src/partials/landing/Navbar.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 
 const Navbar = () => {
   return (
     <>
     <div className="navbar bg-base-100">
-  <div className="navbar-start">
+    <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg

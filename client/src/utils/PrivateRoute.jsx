9c3c2bcdc2f6fa91751d565d1baeab0a2879e2fe
Ankush Makkar <ankushmakkar108@gmail.com>
2024-07-28 13:06:09 +0530

    yo

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 const PrivateRoute = () => {
-    let { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
     return user ? <Outlet /> : <Navigate to="/login" />;
 };

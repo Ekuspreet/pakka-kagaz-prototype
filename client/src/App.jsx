077fdb66186d63149c0b042d39e1287b037eaa96
chandanbir <chandanbirsingh9@gmail.com>
2024-07-28 23:42:59 +0530

    chore: Update npm dependency to latest stable version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
-
-import './App.css'
-import Landing from './pages/Landing.jsx'
-
+import "./App.css";
+import Landing from "./pages/Landing.jsx";
 
 function App() {
-
-
-  return (
-    <>
-     <Landing/>
-    </>
-  )
+    return (
+        <>
+            <Landing />
+        </>
+    );
 }
 
-export default App
+export default App;

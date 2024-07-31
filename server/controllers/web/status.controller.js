436ac62b5f8936f20392c603be15d57aecfb84a7
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-08-01 01:35:50 +0530

    fix: route labelling , use method

--- server/controllers/web/status.controller.js
+++ server/controllers/web/status.controller.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // Define your route handler
-router.get("/status", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Server is up and running!");
 });
 
--- server/routes/web.routes.js
+++ server/routes/web.routes.js
@@ -6,8 +6,8 @@ const landingPageController = require("@controllers/web/landingPage.controller")
 const authPageController = require("@controllers/web/authPage.controller");
 module.exports = (app) => {
   // all controllers will export an instance of Router Class. (Not the actual functions).
-  app.get("/status", statusController);
-  app.get("/", landingPageController);
-  app.get("/auth", authPageController);
+  app.use("/status", statusController);
+  app.use("/", landingPageController);
+  app.use("/auth", authPageController);
 }
 // Ps. This is exporting a function which is then called in the server/index.js file. I'm using the app instance here.

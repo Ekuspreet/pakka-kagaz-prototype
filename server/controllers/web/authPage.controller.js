2ea5106d0b5d1e093c8e6aa425c121e155bba059
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-08-01 02:04:21 +0530

    auth forms connected

--- server/controllers/web/authPage.controller.js
+++ server/controllers/web/authPage.controller.js
@@ -5,7 +5,14 @@ const router = express.Router();
 // Define your route handler
 router.get("/login", (req, res) => {
   res.render("login", {
-    layout : "layouts/auth.layout.ejs"
+    layout : "layouts/auth.layout.ejs",
+    title : "LOGIN"
+  });
+});
+router.get("/register", (req, res) => {
+  res.render("register", {
+    layout : "layouts/auth.layout.ejs",
+    title : "REGISTER"
   });
 });
 

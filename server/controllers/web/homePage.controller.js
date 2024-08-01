f238231755ecc9e4ba174d21b9abf97bc1e8ecdf
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-08-01 23:03:37 +0530

    auth

--- server/controllers/web/homePage.controller.js
+++ server/controllers/web/homePage.controller.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
+const auth = require("@middlewares/web.auth.middleware");
 // Define your route handler
 router.get("/", (req, res) => {
+  const user = auth(req, res);
   res.render("home", {
     layout : "layouts/profile.layout.ejs",
+    user : user,
   });
 });
 

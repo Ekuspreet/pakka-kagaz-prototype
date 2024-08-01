0f2b8b0175e46f1f1c3e43f0431586be739bf2c7
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-08-01 23:40:14 +0530

    completed auth

--- server/controllers/web/homePage.controller.js
+++ server/controllers/web/homePage.controller.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const auth = require("@middlewares/web.auth.middleware");
 // Define your route handler
-router.get("/", (req, res) => {
-  const user = auth(req, res);
+router.get("/", async (req, res) => {
+  const user = await auth(req, res);
+  console.log(user);
+  
   res.render("home", {
     layout : "layouts/profile.layout.ejs",
     user : user,

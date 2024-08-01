410309f5fad9c020361e7932a44474b62b1bc005
chandanbir <chandanbirsingh9@gmail.com>
2024-08-01 23:33:11 +0530

    update

--- server/controllers/web/homePage.controller.js
+++ server/controllers/web/homePage.controller.js
@@ -5,8 +5,27 @@ const router = express.Router();
 const auth = require("@middlewares/web.auth.middleware");
 // Define your route handler
 router.get("/", (req, res) => {
-  const user = auth(req, res);
+  // const user = auth(req, res);
+  const user = {};
+  user.name = "John Doe";
   res.render("home", {
+    layout : "layouts/profile.layout.ejs",
+    user : user,
+  });
+});
+
+router.get("/add-new", (req, res) => {
+  const user = {};
+  user.name = "John Doe";
+  res.render("addnew", {
+    layout : "layouts/profile.layout.ejs",
+    user : user,
+  });
+});
+router.get("/getdetails", (req, res) => {
+  const user = {};
+  user.name = "John Doe";
+  res.render("getdetails", {
     layout : "layouts/profile.layout.ejs",
     user : user,
   });

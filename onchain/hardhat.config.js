c1d80b2abb53e86a60425fd15fcd19e9293f0ad5
Ankush Makkar <ankushmakkar108@gmail.com>
2024-08-01 22:22:58 +0530

    added amoy/matic

--- onchain/hardhat.config.js
+++ onchain/hardhat.config.js
@@ -6,8 +6,23 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: process.env.ALCHEMY_TESTNET_RPC_URL,
-      accounts: [process.env.TESTNET_PRIVATE_KEY],
+      url: process.env.ALCHEMY_SEPOLIA_ETH_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
+    eth: {
+      url: process.env.ALCHEMY_MAINNET_ETH_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    matic: {
+      url: process.env.ALCHEMY_MAINNET_MATIC_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    amoy: {
+      url: process.env.ALCHEMY_TESTNET_MATIC_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
 };

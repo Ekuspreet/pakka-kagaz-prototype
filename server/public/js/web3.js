e68697e13ba43ee8ee666399efeeb56c75f0e852
Ankush Makkar <ankushmakkar108@gmail.com>
2024-08-01 23:58:47 +0530

    issue and get details working

--- server/public/js/web3.js
+++ server/public/js/web3.js
@@ -605,17 +605,17 @@ async function connectWallet() {
   console.log(signer);
   connected = true;
 }
-document
-  .getElementById("issue-idea-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const title = document.getElementById("title").value;
-    const inventor = document.getElementById("inventor").value;
-    const description = document.getElementById("description").value;
-    const fileUrl = document.getElementById("fileUrl").value;
+// document
+//   .getElementById("issue-idea-form")
+//   .addEventListener("submit", async (event) => {
+//     event.preventDefault();
+//     const title = document.getElementById("title").value;
+//     const inventor = document.getElementById("inventor").value;
+//     const description = document.getElementById("description").value;
+//     const fileUrl = document.getElementById("fileUrl").value;
 
-    await issueIdea(title, inventor, description, fileUrl);
-  });
+//     await issueIdea(title, inventor, description, fileUrl);
+//   });
 
 async function issueIdea(title, inventor, description, fileUrl) {
   try {
@@ -627,10 +627,25 @@ async function issueIdea(title, inventor, description, fileUrl) {
   }
 }
 
+document
+  .getElementById("get-idea-form")
+  .addEventListener("submit", async (event) => {
+    console.log("Fetching idea");
+    event.preventDefault();
+    const number = document.getElementById("ideaId").value;
+    console.log("Fetching idea with ID:", number);
+
+    await getIdea(number);
+  });
+
 async function getIdea(id) {
   try {
     const idea = await contract.getIdea(id);
     console.log("Idea details:", idea);
+    document.getElementById("show-idea-1").innerText = idea.title;
+    document.getElementById("show-idea-2").innerText = idea.inventor;
+    document.getElementById("show-idea-3").innerText = idea.description;
+    document.getElementById("show-idea-4").innerText = idea.fileUrl;
   } catch (error) {
     console.error("Error fetching idea:", error);
   }
